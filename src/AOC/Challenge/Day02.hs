e2f09c2770c543161f18fb708d2eb54b23682956
moorts <moritz.gutfleisch@gmx.de>
2022-12-02 10:42:35 +0100

    Day2

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -22,22 +22,43 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day02 (
-    -- day02a
-  -- , day02b
+     day02a
+  , day02b
   ) where
 
 import           AOC.Prelude
 
+parseChoice :: String -> (Int, Int)
+parseChoice s = (toNum $ head s,  toNum $ last s)
+
+toNum :: Char -> Int
+toNum 'X' = 1
+toNum 'Y' = 2
+toNum 'Z' = 3
+toNum 'A' = 1
+toNum 'B' = 2
+toNum 'C' = 3
+toNum _ = -1
+
+score1 :: (Int, Int) -> Int
+score1 (left, right) = right + (right - left + 1) `mod` 3 * 3
+
+score2 :: (Int, Int) -> Int
+score2 (left, strat) = (strat - 1) * 3 + (left + strat) `mod` 3 + 1
+
+solve :: ((Int, Int) -> Int) -> [String] -> Int
+solve win = sum . map (win . parseChoice)
+
 day02a :: _ :~> _
 day02a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve score1
     }
 
 day02b :: _ :~> _
 day02b = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve score2
     }
