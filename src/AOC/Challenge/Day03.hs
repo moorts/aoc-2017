554a673a24052db922f3c5458e8cd7b011b18057
moorts <moritz.gutfleisch@gmx.de>
2022-12-03 12:02:56 +0100

    Day 3

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -22,22 +22,44 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day03 (
-    -- day03a
-  -- , day03b
+    day03a
+  , day03b
   ) where
 
 import           AOC.Prelude
+import Data.Char (ord)
+import Data.List.Split (splitEvery)
+import Data.Set (intersection, fromList)
+import qualified Data.Set as M
+score :: Char -> Int
+score c
+    | ord c > ord 'Z' = (ord c - ord 'a') + 1
+    | otherwise = 26 + (ord c - ord 'A') + 1
+
+-- Actual Challenge Code
+process :: [Char] -> Int
+process line = score . head $ filter (\x -> x `elem` right) left
+    where (left, right) = splitAt (length line `div` 2) line
+
+-- This one won't work with current parsing (remove map fromList .) to make it work
+processGroup :: [[Char]] -> Int
+processGroup [first, second, third] = score . head $ filter (\x -> x `elem` second && x `elem` third) first
+
+-- Bit more elegant version using set intersections
+processGroup' :: [Set Char] -> Int
+processGroup' (x:xs) = score $ M.elemAt 0 $ foldl' intersection x xs
+
 
 day03a :: _ :~> _
 day03a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . map process
     }
 
 day03b :: _ :~> _
 day03b = MkSol
-    { sParse = Just
+    { sParse = Just . splitEvery 3 . map fromList . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . map processGroup'
     }
